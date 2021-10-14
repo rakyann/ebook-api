4029d19e2f60620feb77fd93f96231c71e374498
bagassurahman <bagassurahman2@gmail.com>
2021-10-14 08:20:04 +0700

    Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -22,69 +24,52 @@ class AuthController extends Controller
         ];
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    // R E G I S T E R
+  public function register(Request $request) {
+    $fields = $request->validate([
+        'name' => 'required|string',
+        'email' => 'required|string|unique:users,email',
+        'password' => 'required|string|confirmed'
+    ]);
+    $user = User::create([
+        'name' => $fields['name'],
+        'email' => $fields['email'],
+        'password' => bcrypt($fields['password'])
+    ]);
+    $token = $user->createToken('myapptoken')->plainTextToken;
+    $response = [
+        'user' => $user,
+        'token' => $token
+    ];
+    return response($response, 201);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  // L O G I N
+  public function login(Request $request) {
+      $fields = $request->validate([
+          'email' => 'required|string',
+          'password' => 'required|string'
+      ]);
+      $user = User::where('email', $fields['email'])->first();
+      if(!$user || !Hash::check($fields['password'], $user->password)) {
+          return response([
+              'message' => 'Bad creds'
+          ], 401);
+      }
+      $token = $user->createToken('myapptoken')->plainTextToken;
+      $response = [
+          'user' => $user,
+          'token' => $token
+      ];
+      return response($response, 201);
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+  // L O G O U T
+  public function logout(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $request->user()->currentAccessToken()->delete();
+        return [
+            'message' => 'Logged out'
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\AuthorController;
+// use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 // Route::get('/book', [BookController::class, 'index']);
 // Route::post('/book', [BookController::class, 'store']);
@@ -25,3 +31,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 // Route::delete('/book/{id}', [BookController::class, 'destroy']);
 Route::resource('book', BookController::class)->except('edit', 'create');
 Route::resource('author', AuthorController::class)->except('edit', 'create');
+
+//  login register
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// protected
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('book', BookController::class)->except('edit', 'create');
+    Route::resource('author', AuthorController::class)->except('edit', 'create');
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
